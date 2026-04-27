ac29eb223483291d8dde95dd18a9de05ef214f7d
bsc-com-18-24 <bsc-com-18-24@unima.ac.mw>
2026-04-27 22:12:58 +0200

    LAB 9 POLYMORPHISM

--- mainh.cpp
+++ mainh.cpp
@@ -8,11 +8,15 @@ int main (){
     Rectangle rect;
     Triangle trgl;
 
-    rect.SetValues(4,5);
-    trgl.SetValues(4,5);
+    Polygon* pPoly1 = &rect;
+    Polygon* pPoly2 = &trgl;
 
-    cout<<rect.Area()<<endl;
-    cout<<trgl.Area()<<endl;
+    pPoly1->SetValues(4,5);
+    pPoly2->SetValues(4,5);
+   
+
+    cout<<rect.Area()<<'\n';
+    cout<<trgl.Area()<<'\n';
 
     return 0;
 }
